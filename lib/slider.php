6b3a409af6bbbb88e1cbb38a111721a5a0697911
nooldey <openzws@163.com>
2016-01-04 12:18:26 +0800

    slides code rebuilt in php

--- lib/slider.php
+++ lib/slider.php
@@ -1,72 +1,34 @@
 <section id="top-carousel" class="carousel slide" data-interval="3500" data-ride="carousel">
       <div class="carousel-inner" role="listbox">
-
-        <div class="item active">
-          <?php /*main image*/
-            $img = zti_opt('zti_slide1_img');
-            $default = get_bloginfo('template_directory') .'/images/slide.png';
-            $imgurl = $img ? $img : $default;
-            echo "<img class='slide-img' src='$imgurl' alt='First slide'>";
-          ?>
-          <div class="carousel-caption hidden-xs">
-          <?php /*carousel-caption*/
-            $desc = zti_opt('zti_slide1_desc');
-            $link = zti_opt('zti_slide1_link');
-            $default = '';
-            $h3 = "<h3 class='slide-title'>$desc</h3>";
-            $button = "<a target='_blank' role='button' href='$link'><span class='btn btn-lg btn-primary'>View More</span></a>";
-            $desc_out = $desc ? $h3 : $default;
-            echo $desc_out;
-            $link_out = $link ? $button : $default;
-            echo $link_out;
-          ?>
-          </div>
-        </div>
-
-        <div class="item">
-          <?php /*main image*/
-            $img = zti_opt('zti_slide2_img');
-            $default = get_bloginfo('template_directory') .'/images/slide.png';
-            $imgurl = $img ? $img : $default;
-            echo "<img class='slide-img' src='$imgurl' alt='Second slide'>";
-          ?>
-          <div class="carousel-caption hidden-xs">
-          <?php /*carousel-caption*/
-            $desc = zti_opt('zti_slide2_desc');
-            $link = zti_opt('zti_slide2_link');
-            $default = '';
-            $h3 = "<h3 class='slide-title'>$desc</h3>";
-            $button = "<a target='_blank' role='button' href='$link'><span class='btn btn-lg btn-primary'>View More</span></a>";
-            $desc_out = $desc ? $h3 : $default;
-            echo $desc_out;
-            $link_out = $link ? $button : $default;
-            echo $link_out;
-          ?>
-          </div>
-        </div>
-
-        <div class="item">
-          <?php /*main image*/
-            $img = zti_opt('zti_slide3_img');
-            $default = get_bloginfo('template_directory') .'/images/slide.png';
-            $imgurl = $img ? $img : $default;
-            echo "<img class='slide-img' src='$imgurl' alt='Third slide'>";
-          ?>
-          <div class="carousel-caption hidden-xs">
-          <?php /*carousel-caption*/
-            $desc = zti_opt('zti_slide3_desc');
-            $link = zti_opt('zti_slide3_link');
-            $default = '';
-            $h3 = "<h3 class='slide-title'>$desc</h3>";
-            $button = "<a target='_blank' role='button' href='$link'><span class='btn btn-lg btn-primary'>View More</span></a>";
-            $desc_out = $desc ? $h3 : $default;
-            echo $desc_out;
-            $link_out = $link ? $button : $default;
-            echo $link_out;
-          ?>
-          </div>
-        </div>
-
+<?php
+  $i = 0;
+  while ( $i<= 2) {
+    /*修改2可显示X+1张幻灯片*/
+    $i++;
+    if ($i == 1) {
+      $item .= "<div class='item active'>";
+    }else{
+      $item .= "<div class='item'>";
+    }
+    /*main image*/
+      $key = 'zti_slide'.$i;
+      $img = zti_opt($key.'_img');
+      $default_img = get_bloginfo('template_directory') .'/images/slide.png';
+      $imgurl = $img ? $img : $default_img;
+      $item .= "<img class='slide-img' src='$imgurl' alt='slideimg'>";
+      $item .= "<div class='carousel-caption hidden-xs'>";
+    /*carousel-caption*/
+      $desc = zti_opt($key.'_desc');
+      $link = zti_opt($key.'_link');
+      $default = '';
+      $h3 = "<h3 class='slide-title'>".$desc."</h3>";
+      $button = "<a target='_blank' role='button' href='".$link."'><span class='btn btn-lg btn-primary'>View More</span></a>";
+      $item .= $desc ? $h3 : $default;
+      $item .= $link ? $button : $default;
+      $item .= "</div></div>";
+  }
+  echo $item;
+?>
       </div><!--carousel-inner end-->
 
       <a class="left carousel-control transition" href="#top-carousel" role="button" data-slide="prev">
